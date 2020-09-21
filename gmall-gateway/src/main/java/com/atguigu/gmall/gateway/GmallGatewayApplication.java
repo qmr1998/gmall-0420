628abc6c9340a60655db4210bf148f27abef53bc
ljh <ljh5657985@126.com>
2020-09-21 22:46:45 +0800

    gateway缃戝叧妯″潡涓庡墠绔痑dmin妯″潡鍒涘缓

---
+++ gmall-gateway/src/main/java/com/atguigu/gmall/gateway/GmallGatewayApplication.java
@@ -0,0 +1,16 @@
+package com.atguigu.gmall.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+
+@SpringBootApplication
+@EnableDiscoveryClient // 注册服务  默认是开启的，可以不写
+public class GmallGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GmallGatewayApplication.class, args);
+    }
+
+}
